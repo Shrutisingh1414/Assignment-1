e5028ad5ebfd728791b0b76d085c05428f5bac91
Shrutisingh1414 <shrutisinghpac@gmai.com>
2024-02-20 13:35:28 +0530

    updated

---
+++ Q.7 .php
@@ -0,0 +1,13 @@
+<?php
+$currentTemperature=30;//example temperature
+if($currentTemperature>25){
+    echo "It is a sunny day";
+}else{
+   echo "Consider taking an umbrella";
+}
+// Example usage:
+$currentTemperature = 30; // Assuming the current temperature is 30°C
+$recommendation = get Weather Recommendation($currentTemperature);
+echo $recommendation;
+?>
+
